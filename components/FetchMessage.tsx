5df561216d4fe4f2a2c71c0c1e3b8e45873f8cc0
Roberth Parreiras <roberthparreiras@hotmail.com>
2024-10-17 09:50:06 -0300

    changes made in the code

--- components/FetchMessage.tsx
+++ components/FetchMessage.tsx
@@ -1,12 +1,12 @@
 export default async function FetchMessage() {
-  const res = await fetch("http://127.0.0.1:5000", {
-    cache: "no-store",
-  });
-  const message: { msg: string } = await res.json();
-
+  // const res = await fetch("http://127.0.0.1:5000", {
+  //   cache: "no-store",
+  // });
+  // const message: { msg: string } = await res.json();
+  const message: { msg: string } = { msg: "" };
   return (
     <div className="text-2xl">
-      {message.msg ? <p>{message.msg}</p> : <p>Deu tudo errado!</p>}
+      {message ? <p>{message.msg}</p> : <p>Deu tudo errado!</p>}
     </div>
   );
 }
